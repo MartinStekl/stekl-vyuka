c68148780498bca2d99156318d54452d5e579b8d
MartinStekl <35061925+MartinStekl@users.noreply.github.com>
2021-11-15 15:36:56 +0100

    stekl-vyuka

---
+++ src/App_.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+//import BasicExample from "./router/RouterExample.js";
+import Kalkulacka from "./kalkulacka.js";
+import Cviceni_1 from "./example1.js";
+//import Data from "./calculator/data.json";
+
+export default class App extends Component {
+  render() {
+    return <Cviceni_1 />;
+    <logo />;
+  }
+}
--- src/example1.js
+++ src/example1.js
@@ -6,41 +6,46 @@ import {
   Route,
   Link,
   useHistory,
+  useParams,
+  useRouteMatch,
 } from "react-router-dom";
 
 import hory from "./hory4.jpg";
 import h from "./h.png";
 import v from "./v.png";
 import ch from "./ch.png";
+import hm from "./hm.png";
+import vm from "./vm.png";
+import chm from "./chm.png";
 
-export default function Cviceni_1() {
-  //const history = useHistory();
+export default function CviceniMoto() {
+  //let { path, url } = useRouteMatch();
   return (
     <Router>
       <div>
         <ul>
           <li>
-            <Link to="/">Domů</Link>
-          </li>
-          <li>
-            <Link to="/o">O aplikaci</Link>
+            <Link to="/d">Domů</Link>
           </li>
           <li>
             <Link to="/auta">Auta</Link>
+          </li>
+          <li>
+            <Link to="/motorky">Motorky</Link>
           </li>
         </ul>
 
         <hr />
 
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/d">
             <Domu />
-          </Route>
-          <Route path="/o">
-            <O />
           </Route>
           <Route path="/auta">
             <Auta />
+          </Route>
+          <Route path="/motorky">
+            <Motorky />
           </Route>
         </Switch>
       </div>
@@ -48,10 +53,96 @@ export default function Cviceni_1() {
   );
 }
 
+function Auta() {
+  let { path, url } = useRouteMatch();
+  const menu = [
+    { adresa: "auta_1", popis: "Hezký auta" },
+    { adresa: "auta_2", popis: "Vošklivý auta" },
+    { adresa: "auta_3", popis: "Chytrý auta" },
+  ];
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      {menu.map((polozka) => {
+        return (
+          <li>
+            <Link to={`${url}/${polozka.adresa}`}>{polozka.popis}</Link>
+          </li>
+        );
+      })}
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Vyberte si auto dle libosti.</h3>
+        </Route>
+        <Route path={`${path}/:idAuta`}>
+          <VetveniAuta />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Motorky() {
+  let { path, url } = useRouteMatch();
+  const menu = [
+    { adresa: "moto_1", popis: "Hezký moto" },
+    { adresa: "moto_2", popis: "Vošklivý moto" },
+    { adresa: "moto_3", popis: "Chytrý moto" },
+  ];
+
+  return (
+    <div>
+      <h2>Moto</h2>
+      {menu.map((polozka) => {
+        return (
+          <li>
+            <Link to={`${url}/${polozka.adresa}`}>{polozka.popis}</Link>
+          </li>
+        );
+      })}
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Vyberte si motorku dle libosti.</h3>
+        </Route>
+        <Route path={`${path}/:idMoto`}>
+          <VetveniMoto />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function VetveniMoto() {
+  let { idMoto } = useParams();
+
+  return (
+    <div>
+      {idMoto === "moto_1" && <Moto_1 />}
+      {idMoto === "moto_2" && <Moto_2 />}
+      {idMoto === "moto_3" && <Moto_3 />}
+    </div>
+  );
+}
+
+function VetveniAuta() {
+  let { idAuta } = useParams();
+
+  return (
+    <div>
+      {idAuta === "auta_1" && <Auta_1 />}
+      {idAuta === "auta_2" && <Auta_2 />}
+      {idAuta === "auta_3" && <Auta_3 />}
+    </div>
+  );
+}
+
 function Domu() {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Domů</h2>
       <p>a jsme doma</p>
       <div>
         <img src={hory} />
@@ -60,19 +151,9 @@ function Domu() {
   );
 }
 
-function O() {
-  return (
-    <div>
-      <h2>O aplikaci</h2>
-      <p>keci, keci, kecičky</p>
-      <p>táta koupil botičky</p>
-    </div>
-  );
-}
-
 function Auta_1() {
   const history = useHistory();
-  const handleClick = () => history.push("/auta 2");
+  const handleClick = () => history.push("auta_2");
 
   return (
     <div>
@@ -87,8 +168,8 @@ function Auta_1() {
 
 function Auta_2() {
   const history = useHistory();
-  const handleClick1 = () => history.push("/auta 1");
-  const handleClick3 = () => history.push("/auta 3");
+  const handleClick1 = () => history.push("auta_1");
+  const handleClick3 = () => history.push("auta_3");
 
   return (
     <div>
@@ -104,7 +185,7 @@ function Auta_2() {
 
 function Auta_3() {
   const history = useHistory();
-  const handleClick2 = () => history.push("/auta 2");
+  const handleClick2 = () => history.push("auta_2");
 
   return (
     <div>
@@ -117,40 +198,49 @@ function Auta_3() {
   );
 }
 
-function Auta() {
+function Moto_1() {
+  const history = useHistory();
+  const handleClick = () => history.push("moto_2");
+
   return (
     <div>
-      <h2>auta</h2>
+      <h2>Hezký motorky</h2>
+      <div>
+        <img src={hm} />
+      </div>
+      <button onClick={handleClick}> vošklivky </button>
+    </div>
+  );
+}
 
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/auta 1">Hezký auta</Link>
-            </li>
-            <li>
-              <Link to="/auta 2">Vošklivý auta</Link>
-            </li>
-            <li>
-              <Link to="/auta 3">Chytrý auta</Link>
-            </li>
-          </ul>
+function Moto_2() {
+  const history = useHistory();
+  const handleClick1 = () => history.push("moto_1");
+  const handleClick3 = () => history.push("moto_3");
 
-          <hr />
+  return (
+    <div>
+      <h2>Vošklivý motorky</h2>
+      <div>
+        <img src={vm} />
+      </div>
+      <button onClick={handleClick1}> hezký </button>
+      <button onClick={handleClick3}> chytrý </button>
+    </div>
+  );
+}
 
-          <Switch>
-            <Route path="/auta 1">
-              <Auta_1 />
-            </Route>
-            <Route path="/auta 2">
-              <Auta_2 />
-            </Route>
-            <Route path="/auta 3">
-              <Auta_3 />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+function Moto_3() {
+  const history = useHistory();
+  const handleClick2 = () => history.push("moto_2");
+
+  return (
+    <div>
+      <h2>Chytrý motorky</h2>
+      <div>
+        <img src={chm} />
+      </div>
+      <button onClick={handleClick2}> vošklivý </button>
     </div>
   );
 }
